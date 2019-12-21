5964fa57ba51c851d3edc26cd8213a5175bf8dd6
Marcin Erdmann <erdi84@gmail.com>
2019-12-21 20:16:42 +0100

    Don't unnecessarily download all selenium driver jars but only the one for chrome

--- src/test/resources/GebConfig.groovy
+++ src/test/resources/GebConfig.groovy
@@ -1,8 +1,1 @@
-import org.openqa.selenium.chrome.ChromeDriver
-import org.openqa.selenium.chrome.ChromeOptions
-
 baseUrl = "https://react-redux.realworld.io/"
-
-driver = {
-    new ChromeDriver(new ChromeOptions())
-}
