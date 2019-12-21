c9e91643b643e31ad2b2adf1661cbe0be81aa31b
Marcin Erdmann <erdi84@gmail.com>
2019-12-21 20:08:41 +0100

    Switch to using Gradle webdriver-binaries plugin

--- src/test/resources/GebConfig.groovy
+++ src/test/resources/GebConfig.groovy
@@ -1,10 +1,8 @@
-import io.github.bonigarcia.wdm.WebDriverManager
 import org.openqa.selenium.chrome.ChromeDriver
 import org.openqa.selenium.chrome.ChromeOptions
 
 baseUrl = "https://react-redux.realworld.io/"
 
 driver = {
-    WebDriverManager.chromedriver().setup()
     new ChromeDriver(new ChromeOptions())
 }
