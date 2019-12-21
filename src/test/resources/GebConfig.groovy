b08b1991957f08f04d5cd429be0a5cbac5471d35
Marcin Erdmann <erdi84@gmail.com>
2019-12-21 22:31:45 +0100

    Setup driving a testcontainers based browser

--- src/test/resources/GebConfig.groovy
+++ src/test/resources/GebConfig.groovy
@@ -1,1 +1,16 @@
+import org.openqa.selenium.chrome.ChromeOptions
+import org.testcontainers.containers.BrowserWebDriverContainer
+
 baseUrl = "https://react-redux.realworld.io/"
+
+environments {
+    testcontainers {
+        driver = {
+            def container = new BrowserWebDriverContainer<>()
+                .withCapabilities(new ChromeOptions())
+
+            container.start()
+            container.webDriver
+        }
+    }
+}
