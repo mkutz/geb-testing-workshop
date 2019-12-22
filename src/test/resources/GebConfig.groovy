f2b36f0d4ac8d668a2a5f4af808b4a68ed07db87
Marcin Erdmann <erdi84@gmail.com>
2019-12-22 11:35:42 +0100

    Change config to run against a local, dockerised instance of the app

--- src/test/resources/GebConfig.groovy
+++ src/test/resources/GebConfig.groovy
@@ -1,16 +1,32 @@
 import org.openqa.selenium.chrome.ChromeOptions
 import org.testcontainers.containers.BrowserWebDriverContainer
+import org.testcontainers.containers.GenericContainer
 
-baseUrl = "https://react-redux.realworld.io/"
+baseUrl = "http://localhost:5000/"
 
 environments {
     testcontainers {
+        baseUrl = "http://frontend:5000/"
+
         driver = {
             def container = new BrowserWebDriverContainer<>()
                 .withCapabilities(new ChromeOptions())
 
             container.start()
+            addContainerToNetwork(container, "geb-testing-workshop")
             container.webDriver
         }
     }
+}
+
+void addContainerToNetwork(GenericContainer container, String networkName) {
+    def network = container.dockerClient.listNetworksCmd()
+        .withNameFilter(networkName)
+        .exec()
+        .first()
+
+    container.dockerClient.connectToNetworkCmd()
+        .withContainerId(container.containerId)
+        .withNetworkId(network.id)
+        .exec()
 }
